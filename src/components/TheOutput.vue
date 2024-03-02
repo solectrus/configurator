90b700fbf9731d1174eeb89f59a20de668fd44bd
Georg Ledermann <georg@ledermann.dev>
2024-03-02 19:30:56 +0100

    highlight

--- src/components/TheOutput.vue
+++ src/components/TheOutput.vue
@@ -1,44 +1,27 @@
 <script setup lang="ts">
-import { watch, nextTick } from 'vue'
-
 import { useSurveyStore } from '@/stores/survey'
 
 const surveyStore = useSurveyStore()
-
-import hljs from 'highlight.js/lib/core'
-import yaml from 'highlight.js/lib/languages/yaml'
-import env from 'highlight.js/lib/languages/properties'
-import 'highlight.js/styles/github.css'
-
-hljs.registerLanguage('yaml', yaml)
-hljs.registerLanguage('env', env)
-
-watch(
-  () => surveyStore.composeFile,
-  () => nextTick(() => hljs.highlightAll())
-)
 </script>
 
 <template>
   <div v-if="surveyStore.composeFile" class="font-mono bg-slate-100 space-y-10">
-    <section class="bg-slate-200 px-10 py-5 space-y-5">
+    <section class="bg-slate-200 px-10 py-5 space-y-5 text-sm">
       <header class="sticky top-0 bg-slate-200 py-2 border-b-2 border-green-600">
-        <h1 class="bg-slate-800 text-slate-100 text-sm py-1 px-2 rounded-lg inline-block">
+        <h1 class="bg-slate-800 text-slate-100 py-1 px-2 rounded-lg inline-block">
           docker-compose.yml
         </h1>
       </header>
 
-      <pre><code class="rounded-lg text-sm leading-normal language-yaml">{{ surveyStore.composeFile }}</code></pre>
+      <highlightjs language="yaml" :code="surveyStore.composeFile" />
     </section>
 
-    <section class="bg-slate-200 px-10 py-5 space-y-5">
+    <section class="bg-slate-200 px-10 py-5 space-y-5 text-sm">
       <header class="sticky top-0 bg-slate-200 py-2 border-b-2 border-green-600">
-        <h1 class="bg-slate-800 text-slate-100 text-sm py-1 px-2 rounded-lg inline-block">.env</h1>
+        <h1 class="bg-slate-800 text-slate-100 py-1 px-2 rounded-lg inline-block">.env</h1>
       </header>
 
-      <pre><code class="rounded-lg text-sm leading-normal language-yaml">{{ surveyStore.envFile }}
-        </code>
-      </pre>
+      <highlightjs language="env" :code="surveyStore.envFile" />
     </section>
   </div>
 
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import './styles/app.css'
+import 'highlight.js/styles/github.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import { surveyPlugin } from 'survey-vue3-ui'
+import hljs from 'highlight.js/lib/core'
+import hljsVuePlugin from '@highlightjs/vue-plugin'
+import yaml from 'highlight.js/lib/languages/yaml'
+import env from 'highlight.js/lib/languages/properties'
 
-const pinia = createPinia()
+hljs.registerLanguage('yaml', yaml)
+hljs.registerLanguage('env', env)
 
-createApp(App).use(surveyPlugin).use(pinia).mount('#app')
+const app = createApp(App)
+app.use(hljsVuePlugin)
+app.use(surveyPlugin)
+app.use(createPinia())
+app.mount('#app')
