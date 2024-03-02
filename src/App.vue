2c87f8f5b5ebc9b6bb26a14e820f391e12371ccf
Georg Ledermann <georg@ledermann.dev>
2024-03-02 09:14:42 +0100

    Add output

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import TheWelcome from './components/TheWelcome.vue'
+import TheOutput from './components/TheOutput.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="flex gap-10 w-screen">
+    <TheWelcome class="flex-1" />
+
+    <TheOutput class="flex-1" />
   </main>
 </template>
---
+++ src/components/TheOutput.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="font-mono p-10">
+    <h1>docker-compose.yml</h1>
+
+    <h1>.env</h1>
+  </div>
+</template>
