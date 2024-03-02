cf85c4c9955a9463de61444fa8aeb5803eb0715a
Georg Ledermann <georg@ledermann.dev>
2024-03-02 12:49:17 +0100

    Update TheOutput.vue

--- src/components/TheOutput.vue
+++ src/components/TheOutput.vue
@@ -5,17 +5,24 @@ const surveyStore = useSurveyStore()
 
 <template>
   <div class="font-mono py-10 bg-slate-100 space-y-10">
-    <div class="bg-slate-200 px-10 py-5">
-      <h1 class="text-xl font-bold">Answers</h1>
+    <section class="bg-slate-200 px-10 py-5">
+      <header>
+        <h1 class="text-xl font-bold">Answers</h1>
+      </header>
+
       <pre>{{ surveyStore.answers }}</pre>
-    </div>
+    </section>
 
-    <div class="bg-slate-200 px-10 py-5">
-      <h1 class="text-xl font-bold">docker-compose.yml</h1>
-    </div>
+    <section class="bg-slate-200 px-10 py-5">
+      <header>
+        <h1 class="text-xl font-bold">docker-compose.yml</h1>
+      </header>
+    </section>
 
-    <div class="bg-slate-200 px-10 py-5">
-      <h1 class="text-xl font-bold">.env</h1>
-    </div>
+    <section class="bg-slate-200 px-10 py-5">
+      <header>
+        <h1 class="text-xl font-bold">.env</h1>
+      </header>
+    </section>
   </div>
 </template>
