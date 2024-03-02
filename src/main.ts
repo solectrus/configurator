c71075e575fb8e4fa0358e9ee92d12064e600cd0
Georg Ledermann <georg@ledermann.dev>
2024-03-02 09:11:12 +0100

    add tailwind

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { surveyPlugin } from 'survey-vue3-ui'
+import './styles/app.css'
 
 createApp(App).use(surveyPlugin).mount('#app')
