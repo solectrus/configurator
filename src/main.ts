a1b85f5a796786756c6701db7e1317e53d39e141
Georg Ledermann <georg@ledermann.dev>
2024-03-02 09:14:37 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
+import './styles/app.css'
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import { surveyPlugin } from 'survey-vue3-ui'
-import './styles/app.css'
 
 createApp(App).use(surveyPlugin).mount('#app')
