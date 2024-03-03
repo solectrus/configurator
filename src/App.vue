e170f8fe77b75f487814486b5c574d6d1de20612
Georg Ledermann <georg@ledermann.dev>
2024-03-03 14:06:17 +0100

    styling

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import TheOutput from './components/TheOutput.vue'
 
 <template>
   <main class="lg:flex lg:gap-10 w-screen relative">
-    <TheSurvey class="min-w-fit h-full" />
+    <TheSurvey class="min-w-fit lg:h-screen overflow-y-scroll" />
     <TheOutput class="flex-1 lg:h-screen overflow-y-scroll" />
   </main>
 </template>
