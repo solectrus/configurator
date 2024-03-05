335b817a8c9c12248ba13ec3ce0123baced3afa2
Georg Ledermann <georg@ledermann.dev>
2024-03-05 11:00:21 +0100

    Update nginx.conf
    wip
    
    Update survey.json

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ surveyStore.setSurvey(surveyJson)
 
 <template>
   <main class="lg:flex lg:gap-10 w-screen relative">
-    <TheSurvey class="lg:h-screen overflow-y-scroll max-w-lg w-full" />
+    <TheSurvey class="lg:h-screen overflow-y-scroll max-w-2xl w-full" />
     <TheOutput class="flex-1 lg:h-screen overflow-y-scroll" />
   </main>
 </template>
