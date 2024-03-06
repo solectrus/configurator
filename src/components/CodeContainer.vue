9c3f30a2e6b88f2d5d6cb832b86258fa892730b0
Georg Ledermann <georg@ledermann.dev>
2024-03-06 09:05:35 +0100

    defineprops is macro

--- src/components/CodeContainer.vue
+++ src/components/CodeContainer.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { defineProps, ref, watch } from 'vue'
+import { ref, watch } from 'vue'
 import CopyButton from '@/components/CopyButton.vue'
 
 interface CodeContainerProps {
