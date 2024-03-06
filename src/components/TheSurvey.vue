648bbce85235e77cf10885de867548729af9c988
Georg Ledermann <georg@ledermann.dev>
2024-03-06 16:00:30 +0100

    Update TheSurvey.vue

--- src/components/TheSurvey.vue
+++ src/components/TheSurvey.vue
@@ -29,16 +29,16 @@ const surveyStore = useSurveyStore()
         </p>
       </div>
 
-      <SurveyComponent :model="surveyStore.survey" class="rounded-r" v-else />
+      <SurveyComponent v-else :model="surveyStore.survey" class="rounded-r" />
 
       <div class="mt-2 flex items-end justify-between p-2 pr-0 text-center lg:text-right">
-        <a class="p-2 text-slate-300 underline underline-offset-2" href="https://solectrus.de"
-          >solectrus.de
+        <a href="https://solectrus.de" class="p-2 text-slate-300 underline underline-offset-2">
+          solectrus.de
         </a>
 
         <a
-          class="inline-flex items-center gap-2 space-x-2 rounded p-2 text-sm text-slate-300 hover:bg-red-400 hover:text-white"
           href="/"
+          class="inline-flex items-center gap-2 space-x-2 rounded p-2 text-sm text-slate-300 hover:bg-red-400 hover:text-white"
         >
           <svg
             fill="none"
@@ -53,7 +53,6 @@ const surveyStore = useSurveyStore()
               d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
             />
           </svg>
-
           Neu starten
         </a>
       </div>
