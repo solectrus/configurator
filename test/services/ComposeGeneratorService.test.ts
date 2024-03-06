2ff217835435e333242860eff27a0273a14253aa
Georg Ledermann <georg@ledermann.dev>
2024-03-06 11:32:48 +0100

    Extract test cases

--- test/services/ComposeGeneratorService.test.ts
+++ test/services/ComposeGeneratorService.test.ts
@@ -1,55 +1,6 @@
-import type { Answers } from '@/stores/survey'
 import { ComposeGeneratorService } from '@/services/ComposeGeneratorService'
 
-const testCases: [string, Answers][] = [
-  [
-    'local-full',
-    {
-      installation_type: 'local',
-      battery_vendor: 'battery_senec3',
-      wallbox_vendor: 'wallbox_senec',
-      heatpump_access: 'heatpump_shelly',
-      q_forecast: true,
-      q_updates: true,
-      q_backup: true,
-    },
-  ],
-  [
-    'local-full-mqtt',
-    {
-      installation_type: 'local',
-      battery_vendor: 'battery_other',
-      wallbox_vendor: 'wallbox_other',
-      heatpump_access: 'heatpump_mqtt',
-      q_forecast: true,
-      q_updates: true,
-      q_backup: true,
-    },
-  ],
-  [
-    'local-minimal',
-    {
-      installation_type: 'local',
-      battery_vendor: 'battery_senec3',
-    },
-  ],
-  [
-    'distributed-local',
-    {
-      installation_type: 'distributed',
-      q_distributed_choice: 'local',
-      battery_vendor: 'battery_senec3',
-    },
-  ],
-  [
-    'distributed-cloud',
-    {
-      installation_type: 'distributed',
-      q_distributed_choice: 'cloud',
-      battery_vendor: 'battery_senec3',
-    },
-  ],
-]
+import { testCases } from '../fixtures/answers'
 
 describe('ComposeGeneratorService', () => {
   test.each(testCases)(
