f8661965cef37427361d2781f7827f39aa5bad90
Georg Ledermann <georg@ledermann.dev>
2024-03-06 07:31:54 +0100

    senec-collector env

--- src/services/EnvGeneratorService.ts
+++ src/services/EnvGeneratorService.ts
@@ -53,7 +53,30 @@ export class EnvGeneratorService {
 
   private buildSenecCollectorVariables(): string | undefined {
     if (this.compose.services['senec-collector']) {
-      return this.replaceEnvValues(senecCollectorVariables, {})
+      switch (this.answers.battery_vendor) {
+        case 'battery_senec3':
+          return this.replaceEnvValues(senecCollectorVariables, {
+            SENEC_ADAPTER: 'local',
+            SENEC_HOST: this.answers.senec_host as string,
+            SENEC_LANGUAGE: this.answers.senec_language as string,
+            SENEC_SCHEMA: this.answers.senec_schema as string,
+            SENEC_USERNAME: undefined,
+            SENEC_PASSWORD: undefined,
+            SENEC_SYSTEM_ID: undefined,
+            SENEC_INTERVAL: this.answers.senec_interval as string,
+          })
+        case 'battery_senec4':
+          return this.replaceEnvValues(senecCollectorVariables, {
+            SENEC_ADAPTER: 'cloud',
+            SENEC_HOST: undefined,
+            SENEC_LANGUAGE: undefined,
+            SENEC_SCHEMA: undefined,
+            SENEC_USERNAME: (this.answers.senec_username || 'me@example.com') as string,
+            SENEC_PASSWORD: (this.answers.senec_password || 'secret') as string,
+            SENEC_SYSTEM_ID: this.answers.senec_system_id as string,
+            SENEC_INTERVAL: this.answers.senec_interval_cloud as string,
+          })
+      }
     }
   }
 
@@ -78,12 +101,22 @@ export class EnvGeneratorService {
   // Replace the values of the given environment file content with the given replacements
   // The replacements are expected to be in the format { key: value }
   // Example: replaceEnvValues('FOO=bar\nBAZ=qux', { FOO: 'baz' }) => 'FOO=baz\nBAZ=qux'
-  private replaceEnvValues(envContent: string, replacements: Record<string, string>): string {
+  private replaceEnvValues(
+    envContent: string,
+    replacements: Record<string, string | undefined>,
+  ): string {
     let result = envContent
 
-    Object.entries(replacements).forEach(
-      ([key, value]) => (result = result.replace(new RegExp(`^(${key}=).*`, 'm'), `$1${value}`)),
-    )
+    Object.entries(replacements).forEach(([key, value]) => {
+      const regex = new RegExp(`^${key}=.*$`, 'gm')
+      if (value) {
+        // Replace the value
+        result = result.replace(regex, `${key}=${value}`)
+      } else {
+        // Comment out the line if the value is empty
+        result = result.replace(regex, `# $&`)
+      }
+    })
 
     return result
   }
